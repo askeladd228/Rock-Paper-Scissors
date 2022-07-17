7241ff2b96990663e1871d862df2c0e43507b19d
Nicholas <swaggychoz@gmail.com>
2022-07-17 17:32:03 +0800

    Add buttons and divs

--- javascript.js
+++ javascript.js
@@ -1,3 +1,26 @@
+const rock = document.createElement('button');
+const paper = document.createElement('button');
+const scissors = document.createElement('button');
+
+rock.classList.add('ROCK');
+paper.classList.add('PAPER');
+scissors.classList.add('SCISSORS');
+
+rock.textContent ='ROCK';
+paper.textContent = 'PAPER';
+scissors.textContent = 'SCISSORS';
+
+rock.addEventListener('click', game);
+paper.addEventListener('click', game);
+scissors.addEventListener('click', game);
+
+const container = document.querySelector('.container');
+container.appendChild(rock);
+container.appendChild(paper);
+container.appendChild(scissors);
+
+const div = document.createElement("div");
+
 const array = ['ROCK', 'PAPER', 'SCISSORS'];
 
 //let computer generate a random choice
@@ -11,40 +34,39 @@ let computerPoint = 0;
 
 //create the game mechanism and play a round
 function playRound(playerSelection, computerSelection) {
-  if (playerSelection.toUpperCase() === 'ROCK' && computerSelection === 'SCISSORS') {
-    console.log(`Your point: ${++playerPoint} and Computer\'s point: ${computerPoint}`);  
-    return "You Win! Rock beats Scissors!";
-  } else if(playerSelection.toUpperCase() === 'PAPER' && computerSelection === 'SCISSORS') {
-      console.log(`Your point: ${playerPoint} and Computer\'s point: ${++computerPoint}`);  
-      return "You Lose! Scissors beats Paper!";
-  } else if(playerSelection.toUpperCase() === 'ROCK' && computerSelection === 'PAPER') {
-      console.log(`Your point: ${playerPoint} and Computer\'s point: ${++computerPoint}`); 
-      return "You Lose! Paper beats Rock!";
-  } else if(playerSelection.toUpperCase() === 'SCISSORS' && computerSelection === 'PAPER') {
-      console.log(`Your point: ${++playerPoint} and Computer\'s point: ${computerPoint}`);  
-      return "You Win! Scissors beats Paper";
-  } else if(playerSelection.toUpperCase() === 'PAPER' && computerSelection === 'ROCK') {
-      console.log(`Your point: ${++playerPoint} and Computer\'s point ${computerPoint}`);  
-      return "You Win! Paper beats Rock!";
-  } else if(playerSelection.toUpperCase() === 'SCISSORS' && computerSelection === 'ROCK') {
-      console.log(`Your point: ${playerPoint} and Computer\'s point: ${++computerPoint}`);  
-      return "You Lose! Rock beats Scissors";
-  } else if((playerSelection === "") || (playerSelection === null)) {
-      console.log(`Your point: ${playerPoint} and Computer\'s point: ${++computerPoint}`);  
-      return "You didn't even make a choice!";
-  } else if ((playerSelection.toUpperCase !== 'ROCK') || (playerSelection.toUpperCase !== 'PAPER') || (playerSelection.toUpperCase !== 'SCISSORS') )
-      console.log (`Your point: ${playerPoint} and Computer\'s point: ${++computerPoint}`);
-      return "It's rock, paper, or scissors my dude.";
-  }
+  for (let i = 1; i <= 5; i++) {
+    if (playerSelection === 'ROCK' && computerSelection === 'SCISSORS') {
+      div.textContent = (`Your point: ${++playerPoint} and Computer\'s point: ${computerPoint}` + '\n' + "You Win! Rock beats Scissors!");
+      } else if(playerSelection === 'PAPER' && computerSelection === 'SCISSORS') {
+        div.textContent = (`Your point: ${playerPoint} and Computer\'s point: ${++computerPoint}` + '\n' + "You Lose! Scissors beats Paper!");
+      } else if(playerSelection === 'ROCK' && computerSelection === 'PAPER') {
+        div.textContent = (`Your point: ${playerPoint} and Computer\'s point: ${++computerPoint}` + '\n' + "You Lose! Paper beats Rock!");
+      } else if(playerSelection === 'SCISSORS' && computerSelection === 'PAPER') {
+        div.textContent = (`Your point: ${++playerPoint} and Computer\'s point: ${computerPoint}` + '\n' + "You Win! Scissors beats Paper");  
+      } else if(playerSelection === 'PAPER' && computerSelection === 'ROCK') {
+        div.textContent = (`Your point: ${++playerPoint} and Computer\'s point ${computerPoint}` + '\n' + "You Win! Paper beats Rock!");  
+      } else if(playerSelection === 'SCISSORS' && computerSelection === 'ROCK') {
+        div.textContent = (`Your point: ${playerPoint} and Computer\'s point: ${++computerPoint}` + '\n' + "You Lose! Rock beats Scissors");  
+      } else if((playerSelection === "") || (playerSelection === null)) {
+        div.textContent = (`Your point: ${playerPoint} and Computer\'s point: ${++computerPoint}` + '\n' + "You didn't even make a choice!");  
+      } else {
+        div.textContent = (`Your point: ${playerPoint} and Computer\'s point: ${computerPoint}` + '\n' + "Draw");
+      }
+    container.appendChild(div);
+    }
+  }  
 
 function game() {
-  for (let i = 1; i <= 5; i++) {
-    alert (`Round ${i}`);
-      let playerSelection = prompt('Rock, Paper, Scissors?', '');
-      let computerSelection = computerPlay();
-      console.log(playRound(playerSelection, computerSelection));
- } 
-}
+
+  // for (let i = 1; i <= 5; i++) {
+    // alert (`Round ${i}`);
+    let computerSelection = computerPlay();
+    let playerSelection = this.className;
+    console.log (playRound(playerSelection, computerSelection));
+    // console.log(gameReport());
+ }
+ 
+
 
 function gameReport() {
     if (playerPoint > computerPoint) {
@@ -56,7 +78,15 @@ function gameReport() {
     }  
  }
 
-game();
-console.log(gameReport());
+
+
+
+
+
+
+
+
+
+
 
 
